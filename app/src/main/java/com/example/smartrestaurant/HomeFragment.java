6a9e0c8158208998c4f10f9afb4d1821d7ef85cb
trinhqtkd1994 <trinhqtkd1994@gmail.com>
2021-04-11 17:53:17 +0700

    add

--- app/src/main/java/com/example/smartrestaurant/HomeFragment.java
+++ app/src/main/java/com/example/smartrestaurant/HomeFragment.java
@@ -4,16 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class HomeFragment extends Fragment {
+    private ImageView imgTable;
+    private EditText edtNumberTable;
+    private RecyclerView recyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
+        imgTable = view.findViewById(R.id.img_table);
         return view;
+
+
     }
 }
